73bea23420a1c31fae0534337456ed1a6adc69c9
mnichol3 <nicholson.matt94@gmail.com>
2020-02-18 11:02:33 -0500

    Initial commit; Set up tests & test parse_emission_fname

---
+++ tests/test_ini_template.R
@@ -0,0 +1,9 @@
+context("Basic template .ini file I/O")
+
+test_that("parse_emission_fname returns the proper emissions filename", {
+  expect_equal(parse_emission_fname("rcp45", "emissions/RCP45_emissions.csv"))
+  expect_equal(parse_emission_fname("rcp80", "emissions/RCP80_emissions.csv"))
+  expect_equal(parse_emission_fname("ssp245", "emissions/SSP245_emissions.csv"))
+  expect_equal(parse_emission_fname("ssp434", "emissions/SSP434_emissions.csv"))
+  expect_equal(parse_emission_fname("esm-bell-1000", "emissions/ESM-BELL-1000_emissions.csv"))
+})
