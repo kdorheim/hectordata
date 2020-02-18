0b1a3ec2e19c02eeeaef95927b663e45a2fbe77f
mnichol3 <nicholson.matt94@gmail.com>
2020-02-18 09:32:24 -0500

    Add documentation

--- data-raw/parse_template.R
+++ data-raw/parse_template.R
@@ -1,3 +1,10 @@
+# Read the template Hector input .ini file ("hector_template.ini") into a
+# character vector, then store it as exported package data to be used when
+# creating Hector .ini files for new scenarios
+#
+# Matt Nicholson
+# 17 Feb 2020
+
 # Use `devtools::load_all()` here instead of `library()` because this script is
 # more likely to be run while developing the package, in which case the R
 # package library may have an out of date version of Hector (e.g. with new
