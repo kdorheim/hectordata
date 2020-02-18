739ebc2808c2894e0d0bc958bbc9ae77c4d87f7a
mnichol3 <nicholson.matt94@gmail.com>
2020-02-18 11:39:48 -0500

    Add more test cases for parse_ini_template.R
    * Test parse_ini_fname returns proper Hector .ini filename
    * Test create_scenario_ini RCP45 .ini file; compare to default RCP_45
        .ini file from Hector package
    * Test create_scenario_ini SSP245 .ini file; compare to default SSP245
        .ini file from Hector package

--- tests/testthat/test_ini_template.R
+++ tests/testthat/test_ini_template.R
@@ -7,3 +7,35 @@ test_that("parse_emission_fname returns the proper emissions filename", {
   expect_equal(parse_emission_fname("ssp434"), "emissions/SSP434_emissions.csv")
   expect_equal(parse_emission_fname("esm-bell-1000"), "emissions/ESM-BELL-1000_emissions.csv")
 })
+
+test_that("parse_ini_fname returns the proper Hector input .ini filename", {
+  expect_equal(parse_emission_fname("rcp45"), "hector_rcp45.ini")
+  expect_equal(parse_emission_fname("rcp80"), "hector_rcp80.ini")
+  expect_equal(parse_emission_fname("ssp245"), "hector_ssp245.ini")
+  expect_equal(parse_emission_fname("ssp434"), "hector_ssp434.ini")
+  expect_equal(parse_emission_fname("esm-bell-1000"), "hector_esm-bell-1000.ini")
+})
+
+test_that("Hector .ini file created by create_scenario_ini for RCP45", {
+  scenario <- "rcp45"
+  # Create a new .ini file named "hector_rcp45_test.ini"
+  test_scenario <- paste0(scenario, "_test")
+  create_scenario_ini(test_scenario)
+  # Compare the new RCP45 .ini file to the default Hector RCP45 .ini file
+  new_ini     <- system.file("input", parse_ini_fname(test_scenario), package = "hectordata")
+  control_ini <- system.file("input", "hector_rcp45.ini", package = "hector")
+  expect_true(all.equal(readLines(new_ini), readLines(control_ini)))
+  file.remove(new_ini)  # Remove hector_rcp45_test.ini
+})
+
+test_that("Hector .ini file created by create_scenario_ini for SSP245", {
+  scenario <- "ssp245"
+  # Create a new .ini file named "hector_ssp245_test.ini"
+  test_scenario <- paste0(scenario, "_test")
+  create_scenario_ini(test_scenario)
+  # Compare the new SSP245 .ini file to the default Hector SSP245 .ini file
+  new_ini     <- system.file("input", parse_ini_fname(test_scenario), package = "hectordata")
+  control_ini <- system.file("input", parse_ini_fname(scenario), package = "hector")
+  expect_true(all.equal(readLines(new_ini), readLines(control_ini)))
+  file.remove(new_ini)  # Remove hector_ssp245_test.ini
+})
