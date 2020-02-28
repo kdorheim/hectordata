68d6aca50eefaf914140f6dbafb37fffffd33cdf
mnichol3 <nicholson.matt94@gmail.com>
2020-02-28 17:12:58 -0500

    Add RCMIP-to-Hector variable LUT

--- data-raw/hector_template.R
+++ data-raw/hector_template.R
@@ -1,6 +1,4 @@
-# Read the template Hector input .ini file ("hector_template.ini") into a
-# character vector, then store it as exported package data to be used when
-# creating Hector .ini files for new scenarios
+# Functions to read raw data into internal package data
 #
 # Matt Nicholson
 # 25 Feb 2020
@@ -9,8 +7,16 @@
 # more likely to be run while developing the package, in which case the R
 # package library may have an out of date version of Hector (e.g. with new
 # variables missing).
+
 devtools::load_all()
 
+# Read the template Hector input .ini file
 ini_template <- readLines("data-raw/hector_template.ini")
 
-usethis::use_data(ini_template, internal = TRUE, overwrite = TRUE)
+# Read the RCMIP to Hector emissions Look-Up Table
+rcmip_emissions_lut <- readr::read_csv("data-raw/variable-conversion.csv",
+                                       col_types = readr::cols(.default = "c"))
+
+usethis::use_data(ini_template, rcmip_emissions_lut, internal = TRUE, overwrite = TRUE)
+
+
