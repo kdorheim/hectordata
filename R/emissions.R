502e77c46ccd7111a4a30743f4599da2fda2caae
mnichol3 <nicholson.matt94@gmail.com>
2020-02-28 17:13:31 -0500

    Initial commit

---
+++ R/emissions.R
@@ -0,0 +1,42 @@
+# This file contains functions to construct Hector-readable emissions files
+# from RCMIP emissions files.
+#
+# Matt Nicholson
+# 28 Feb 2020
+
+#' Long RCMIP inputs data.frame
+#'
+#' @param targetfile Character vector, optional; File to read RCMIP input from.
+#' Default is inst/rcmip-inputs.fst
+#' @return
+#' @author Alexey Shiklomanov
+#' @importFrom magrittr %>%
+#' @export
+rcmip_inputs <- function(targetfile = NULL) {
+  if (is.null(targetfile)) {
+    targetfile <- here::here("inst", "rcmip-inputs.fst")
+  }
+  stopifnot(file.exists(targetfile))
+  fst::read_fst(targetfile) %>%
+    tibble::as_tibble()
+}
+
+
+#' Extract emissions data for a given variable from the RCMIP emissions.
+#'
+#' @param hector_var Character vector; Name of the emission variable to retrieve,
+#' in Hector format.
+#' @return Quien sabe
+#' @author Alexey Shiklomanov
+#' @export
+subset_hector_var <- function(input_data, hector_var) {
+  hector_sub <- rcmip2hector_df() %>%
+    dplyr::filter(hector_variable == !!hector_var)
+  stopifnot(nrow(hector_sub) > 0)
+  result <- input_data %>%
+    dplyr::semi_join(hector_sub, c("Variable" = "rcmip_variable"))
+  if (length(unique(result$Variable)) > 1) {
+    stop("Multiple matching variables found for ", hector_var)
+  }
+  result
+}
